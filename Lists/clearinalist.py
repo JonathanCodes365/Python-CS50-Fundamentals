a01ace8cd0f69316c3eaf9fbbe86e712c25c951f
JonathanCodes365 <ngawangt.sherpa777@gmail.com>
2026-08-15 19:29:41 +0545

    Using Method Clear in a list

---
+++ Lists/clearinalist.py
@@ -0,0 +1,11 @@
+#THis here we are doing after deleting in a list.
+
+# Clear() method might be the simplest of all.
+
+#It's job is to clear everything that is in the list.
+
+#So,
+
+numbers = [10,20,30,40,50]
+numbers.clear()
+print(numbers)
